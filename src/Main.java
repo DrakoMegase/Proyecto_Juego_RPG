d37c9ff43a989b6be9090bfb6e972461782f145b
Quim <quimvicoduarte99@gmail.com>
2020-04-10 17:04:37 +0200

    Modificación basica del Main.java
    --Quim

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hola mundo!");   //Esto es una modificiación.
     }
 }
